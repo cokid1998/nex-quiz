3e5de7537509f82c34ffacde264bc09fac5d4f7e
이태관 <cokid98@naver.com>
2026-06-20 18:26:33 +0900

    QuizCard가 리렌더링 됐을 때 질문의 순서가 바뀌는 버그 수정

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -2,8 +2,24 @@ import Feedback from "@/components/Feedback";
 import { shuffleArray } from "@/utils/random";
 import type { RecipeActionContextType, RecipeContextType } from "@/types";
 import { Progress } from "@/components/ui/progress";
+import { useMemo } from "react";
 
 type Props = RecipeContextType & Omit<RecipeActionContextType, "handleReset">;
+/**
+ * 버그 해결 정리
+ * 선택지를 클릭했을 때 원래 순서의 선택지가 shuffleArray 함수 때문에 다시 섞이는 버그가 있음
+ * 즉 처음 보여준 선택지 순서와, 선택지를 클릭한 직후 다시 보여지는 선택지 순서가 다르게 됨
+ * 이 문제의 원인은 correctList가 변경되면서 QuizCard가 리렌더링되고 shuffleArray 다시 호출되면서 일어나는 버그임을 파악함
+ *
+ * 그렇다면 correctList가 바뀔 때 QuizCard가 리렌더링하지 못하게 만들면 되는것이 아닐까?
+ * 그렇지 않음 QuizCard의 isChoice와 isCorrect는 correctList에 의존하고 있음
+ * isChoice와 isCorrect는 UI에 직결된 값들임
+ * 즉 correctList가 변경될 때 QuizCard는 무조건 리렌더링이 돼야함!!
+ *
+ * 그렇기 때문에 위 방법은 좋은 방법이 아님
+ * useMemo를 통해 선택지 순서(shuffleArray의 결과값)를 currentQuiz에만 의존하도록 메모이제이션하는게 맞는 해결방법임
+ * correctList는 선택지 순서와 무관한 값이므로 의존성 배열에 포함되면 안 됨
+ */
 
 export default function QuizCard({
   quiz,
@@ -16,6 +32,11 @@ export default function QuizCard({
   const isChoice = correctList.length === currentQuizIndex + 1;
   const isCorrect = correctList[currentQuizIndex];
   const progressPer = ((currentQuizIndex + 1) / quiz.length) * 100;
+
+  const shuffleChoices = useMemo(
+    () => shuffleArray(currentQuiz.choices),
+    [currentQuiz],
+  );
 
   return (
     <div className="rounded-3xl border border-zinc-200 bg-white p-8 shadow-sm">
@@ -39,7 +60,7 @@ export default function QuizCard({
 
       {/* 선택지 */}
       <div className="mt-10 space-y-3">
-        {shuffleArray(currentQuiz.choices).map((choice, index) => (
+        {shuffleChoices.map((choice, index) => (
           <button
             key={choice}
             className="flex w-full items-center gap-4 rounded-2xl border border-zinc-200 bg-white px-5 py-4 text-left transition-all hover:border-zinc-300 hover:bg-zinc-50 cursor-pointer disabled:cursor-not-allowed disabled:bg-zinc-50 disabled:text-zinc-400 disabled:border-zinc-100"
